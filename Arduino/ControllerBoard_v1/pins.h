4cd93b5533b9b4016cd55975c74f703c58079fe9
Steve Parker <steve.parker43@gmail.com>
2014-07-21 13:24:49 +0100

    Sync

--- Arduino/ControllerBoard_v1/pins.h
+++ Arduino/ControllerBoard_v1/pins.h
@@ -4,8 +4,8 @@
 #include <Arduino.h>
 
 // Define the pins for the OLED display
-#define OLED_MOSI   2
-#define OLED_CLK   3
+#define OLED_MOSI   3
+#define OLED_CLK   4
 #define OLED_DC    11
 #define OLED_CS    12
 #define OLED_RESET 13
@@ -16,7 +16,5 @@
 #define RC2PIN 16
 #define RC3PIN 15
 #define RC4PIN 14
-
-#define RESET 31
 
 #endif
